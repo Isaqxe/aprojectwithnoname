f456b2e2e2b2f0536ed85739b157388e8c3f4433
TryHaverds <isaque.emailforgaming@gmail.com>
2026-08-06 15:30:59 -0300

    Changed Movement Keybinds

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -4,10 +4,10 @@ extends CharacterBody2D
 
 func _physics_process(delta):
 	var direction = Input.get_vector(
-		"ui_left",
-		"ui_right",
-		"ui_up",
-		"ui_down"
+		"move_left",
+		"move_right",
+		"move_up",
+		"move_down"
 	).normalized() 
 
 	velocity = direction * speed
