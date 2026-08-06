aa42c50150934ba5776b4efd8c689a3f81c3d6cb
TryHaverds <isaque.emailforgaming@gmail.com>
2026-08-06 15:27:54 -0300

    Simple Player Movement added (verified)
    Unlike some kind of people say they know, i had to do it myself

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -4,11 +4,11 @@ extends CharacterBody2D
 
 func _physics_process(delta):
 	var direction = Input.get_vector(
-		"move_left",
-		"move_right",
-		"move_up",
-		"move_down"
-	)
+		"ui_left",
+		"ui_right",
+		"ui_up",
+		"ui_down"
+	).normalized() 
 
 	velocity = direction * speed
 
