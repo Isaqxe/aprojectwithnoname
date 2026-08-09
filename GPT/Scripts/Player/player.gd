16446e83f838e2b80d2e3a2c432e047c55b4858e
Isaqxe <isaque.emailforgaming@gmail.com>
2026-08-09 13:47:10 -0300

    Simplify player resource counter integration

--- GPT/Scripts/Player/player.gd
+++ GPT/Scripts/Player/player.gd
@@ -3,11 +3,6 @@ extends CharacterBody2D
 @export var speed: float = 200.0
 @export var resources_collected: int = 0
 
-func _ready() -> void:
-	for molecule in get_tree().get_nodes_in_group("Molecule"):
-		if molecule is Area2D:
-			molecule.body_entered.connect(_on_molecule_body_entered.bind(molecule))
-
 func _physics_process(_delta: float) -> void:
 	var direction = Input.get_vector(
 		"move_left",
@@ -19,10 +14,3 @@ func _physics_process(_delta: float) -> void:
 	velocity = direction * speed
 
 	move_and_slide()
-
-func _on_molecule_body_entered(body: Node2D, molecule: Area2D) -> void:
-	if body != self or not is_instance_valid(molecule):
-		return
-
-	resources_collected += 1
-	molecule.queue_free()
