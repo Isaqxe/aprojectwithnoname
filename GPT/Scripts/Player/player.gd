2b3800b34ff7c9e555795d2367097470b00cf0b8
Isaqxe <isaque.emailforgaming@gmail.com>
2026-08-09 13:47:00 -0300

    Add molecule collection counter to player

--- GPT/Scripts/Player/player.gd
+++ GPT/Scripts/Player/player.gd
@@ -1,8 +1,14 @@
 extends CharacterBody2D
 
 @export var speed: float = 200.0
+@export var resources_collected: int = 0
 
-func _physics_process(delta):
+func _ready() -> void:
+	for molecule in get_tree().get_nodes_in_group("Molecule"):
+		if molecule is Area2D:
+			molecule.body_entered.connect(_on_molecule_body_entered.bind(molecule))
+
+func _physics_process(_delta: float) -> void:
 	var direction = Input.get_vector(
 		"move_left",
 		"move_right",
@@ -13,3 +19,10 @@ func _physics_process(delta):
 	velocity = direction * speed
 
 	move_and_slide()
+
+func _on_molecule_body_entered(body: Node2D, molecule: Area2D) -> void:
+	if body != self or not is_instance_valid(molecule):
+		return
+
+	resources_collected += 1
+	molecule.queue_free()
