bd54dea1fd4ad844c6af3d81ae1c8f35514b795a
Isaqxe <isaque.emailforgaming@gmail.com>
2026-08-28 21:30:24 -0300

    Support inherited cell data in factory

--- GPT/CellSystem/cell_factory.gd
+++ GPT/CellSystem/cell_factory.gd
@@ -5,13 +5,15 @@ extends Node
 
 @export var cell_scene: PackedScene
 
-
-func create_cell(position: Vector2, player_controlled: bool = false) -> Node:
+func create_cell(position: Vector2, player_controlled: bool = false, inherited_data: Dictionary = {}) -> Node:
 	if cell_scene == null:
 		return null
 
 	var new_cell := cell_scene.instantiate()
 	new_cell.global_position = position
 	new_cell.is_player_controlled = player_controlled
+
+	if not inherited_data.is_empty():
+		new_cell.inherited_data = inherited_data
 
 	return new_cell
