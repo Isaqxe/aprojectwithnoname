211b3ae444a1372a10bad830962bef4bb2e43f96
Isaqxe <isaque.emailforgaming@gmail.com>
2026-08-29 11:16:58 -0300

    Center CellSystem test simulation area

--- GPT/CellSystem/test_simulation.gd
+++ GPT/CellSystem/test_simulation.gd
@@ -4,13 +4,15 @@ extends Node2D
 ## CellManager owns initial and continuous spawning.
 ## Space spawns the player cell at world position (0, 0) for debugging.
 
-@export var spawn_area: Rect2 = Rect2(60.0, 60.0, 900.0, 500.0)
+@export var spawn_area: Rect2 = Rect2(-450.0, -300.0, 900.0, 600.0)
 
 @onready var cell_manager: Node = $CellManager
+@onready var resource_spawner: Node2D = $ResourceSpawner
 @onready var debug_label: Label = $Debug
 
 func _ready() -> void:
 	cell_manager.spawn_area = spawn_area
+	resource_spawner.spawn_area = spawn_area
 
 func _process(_delta: float) -> void:
 	if Input.is_key_pressed(KEY_SPACE):
