d5e36dcc84fd4be0f7afb47c697470a1eb3517e0
Isaqxe <isaque.emailforgaming@gmail.com>
2026-09-02 13:42:40 -0300

    Restore simulation clock state when leaving test scene

--- GPT/CellSystem/simulation_tools_v2.gd
+++ GPT/CellSystem/simulation_tools_v2.gd
@@ -12,3 +12,9 @@ func _select_target_from_mouse() -> void:
 			_selected_cell = inspected as Node
 	if not is_instance_valid(_selected_cell):
 		_selected_cell = _query_cell_at_mouse()
+
+func _exit_tree() -> void:
+	Engine.time_scale = DEFAULT_TIME_SCALE
+	var tree: SceneTree = get_tree()
+	if tree != null:
+		tree.paused = false
